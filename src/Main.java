e06a3762fd63996f101b0b496cc7e1fa2e39151a
Natnael <154729622+stealthscripter@users.noreply.github.com>
2024-01-31 13:12:23 +0300

    posgres Handle

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,6 @@ import java.sql.*;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) throws SQLException {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
 
 
 
